b4440bad3218d315f2e5d1a48930fd8c834621ac
HecatoncheirX <muhammadfarrel2020@gail.com>
2025-02-11 12:14:04 +0700

    Pertemuan 2

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+$nama = "M.Farrel";
+$email = "muhammadfarrel2020@gmail.com";
+$jurusan = "Teknik Informatika";
+$pekerjaan = "Mahasiswa";
+$universitas = "UNPAS";
+
+
+echo "<h1> $nama </h1>";
+echo "<h2> $email </h2>";
+echo "<p> Halo nama saya adalah $nama dan saya seorang $pekerjaan $jurusan $universitas </p>";
+?>
