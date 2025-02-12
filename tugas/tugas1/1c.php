c48fc697a4d125f01fa106449f8429e9e829d461
HecatoncheirX <muhammadfarrel2020@gail.com>
2025-02-12 23:15:43 +0700

    Saya Sudah Mengerjakan Tugas 1

---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="box">1</div>
+        </div>
+        <div class="row">
+            <div class="box">2</div>
+            <div class="box">2</div>
+        </div>
+        <div class="row">
+            <div class="box">3</div>
+            <div class="box">3</div>
+            <div class="box">3</div>
+        </div>
+    </div>
+</body>
+</html>
