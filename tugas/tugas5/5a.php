05621db40d8949f88eee13b50709a17401f9ba1c
HecatoncheirX <muhammadfarrel2020@gail.com>
2025-03-25 14:31:31 +0700

    Tugas 5

--- tugas/tugas5/5a.php
+++ tugas/tugas5/5a.php
@@ -88,7 +88,7 @@ $mahasiswa = [
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Mahasiswa</title>
 </head>
 <body>
